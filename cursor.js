db22bc58a92b15d1f35f202b14cc9ef6bec14bc2
Killian <killian.godmont@gmail.com>
2025-01-29 16:23:16 +0100

    correction js

--- cursor.js
+++ cursor.js
@@ -2,15 +2,14 @@ const cursor = document.querySelector('.custom-cursor');
 
 // Suivi de la position de la souris
 document.addEventListener('mousemove', (e) => {
-    cursor.style.left = `${e.clientX - 20}px`; // Centrer le curseur (ajouter un décalage)
-    cursor.style.top = `${e.clientY - 20}px`;  // Centrer le curseur (ajouter un décalage)
+    cursor.style.left = `${e.clientX - 0}px`;
+    cursor.style.top = `${e.clientY - 0}px`;
 });
 
-// Ajouter l'animation lors du clic
 document.addEventListener('click', () => {
     cursor.classList.add('attack');
-    // Enlever la classe 'attack' après l'animation pour pouvoir la rejouer
+
     setTimeout(() => {
         cursor.classList.remove('attack');
-    }, 200); // La durée de l'animation (200 ms)
+    }, 150);
 });
