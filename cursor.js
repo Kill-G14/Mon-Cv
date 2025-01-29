3a1c5fceca9fda9fb1590db548451c65c8248a54
Killian <killian.godmont@gmail.com>
2025-01-29 16:23:32 +0100

    .

--- cursor.js
+++ cursor.js
@@ -1,6 +1,5 @@
 const cursor = document.querySelector('.custom-cursor');
 
-// Suivi de la position de la souris
 document.addEventListener('mousemove', (e) => {
     cursor.style.left = `${e.clientX - 0}px`;
     cursor.style.top = `${e.clientY - 0}px`;
