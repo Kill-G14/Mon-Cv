43ad6de6ed693b597fa1e4ec2f77af812b0c76ef
Killian <killian.godmont@gmail.com>
2025-02-06 15:57:45 +0100

    clean du code

--- cursor.js
+++ cursor.js
@@ -1,29 +1,24 @@
 let currentImageIndex = 0;
-const images = [
-    'images/molgo.jpg',
-    'images/killianGodmont.jpg'
-]
+const images = ["images/molgo.jpg", "images/killianGodmont.jpg"];
 
 function changeImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    document.getElementById("photo").src = images[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  document.getElementById("photo").src = images[currentImageIndex];
 }
 
-
-let cursor = document.createElement('div');
-cursor.classList.add('cursor-animation');
+let cursor = document.createElement("div");
+cursor.classList.add("cursor-animation");
 document.body.appendChild(cursor);
 
-document.body.addEventListener('mousemove', function(e) {
-    cursor.style.left = e.pageX - cursor.offsetWidth / 2 + 'px';
-    cursor.style.top = e.pageY - cursor.offsetHeight / 2 + 'px';
-})
+document.body.addEventListener("mousemove", function (e) {
+  cursor.style.left = e.pageX - cursor.offsetWidth / 2 + "px";
+  cursor.style.top = e.pageY - cursor.offsetHeight / 2 + "px";
+});
 
-document.body.addEventListener('click', function() {
-    cursor.classList.add('cursor-swing');
+document.body.addEventListener("click", function () {
+  cursor.classList.add("cursor-swing");
 
-setTimeout(function() {
-    cursor.classList.remove('cursor-swing');
-}, 300); 
-})
-
+  setTimeout(function () {
+    cursor.classList.remove("cursor-swing");
+  }, 300);
+});
